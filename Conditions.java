0e04c28cad9dc1e83f138c6101e5cdbc0094ca8c
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-11-22 18:46:36 +0530

    New One

---
+++ Conditions.java
@@ -0,0 +1,15 @@
+public class Conditions {
+	public static void main(String[] args) {
+		
+		int num =15;
+		if(num<10) {
+			System.out.println("Yes ! It is less than 10");
+		}
+		else if(num<20){
+			System.out.println("It is less than 20");
+		}
+		else{
+			System.out.println("None of the above");
+		}
+	}
+}
